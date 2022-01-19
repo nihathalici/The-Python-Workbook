aa49d97b20a491b77c3b25ad3d51176569e67fab
Nihat Halici <nihat.halici@gmail.com>
2022-01-19 18:26:57 +0100

    Add files via upload

---
+++ CHPT-01-Intro/Exer-013.py
@@ -0,0 +1,42 @@
+"""
+Exercise 13: Making Change
+
+Consider the software that runs on a self-checkout machine. One task that it must be
+able to perform is to determine how much change to provide when the shopper pays
+for a purchase with cash.
+Write a program that begins by reading a number of cents from the user as an
+integer. Then your program should compute and display the denominations of the
+coins that should be used to give that amount of change to the shopper. The change
+should be given using as few coins as possible. Assume that the machine is loaded
+with pennies, nickels, dimes, quarters, loonies and toonies.
+
+A one dollar coin was introduced in Canada in 1987. It is referred to as a loonie
+because one side of the coin has a loon (a type of bird) on it. The two dollar
+coin, referred to as a toonie, was introduced 9 years later. It’s name is derived
+from the combination of the number two and the name of the loonie.
+"""
+# Author's solution:
+CENTS_PER_TOONIE = 200
+CENTS_PER_LOONIE = 100
+CENTS_PER_QUARTER = 25
+CENTS_PER_DIME = 10
+CENTS_PER_NICKEL = 5
+
+cents = int(input("Enter the number of cents: "))
+
+print(" ", cents // CENTS_PER_TOONIE, "toonies")
+cents = cents % CENTS_PER_TOONIE
+
+print(" ", cents // CENTS_PER_LOONIE, "loonies")
+cents = cents % CENTS_PER_LOONIE
+
+print(" ", cents // CENTS_PER_QUARTER, "quarter")
+cents = cents % CENTS_PER_QUARTER
+
+print(" ", cents // CENTS_PER_DIME, "dime")
+cents = cents % CENTS_PER_DIME
+
+print(" ", cents // CENTS_PER_NICKEL, "nickel")
+cents = cents % CENTS_PER_NICKEL
+
+print(" ", cents, "pennies")
