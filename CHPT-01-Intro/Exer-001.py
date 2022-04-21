331fac3a5746eec23a01a6e31840d9ec8f41d965
Nihat Halici <nihat.halici@gmail.com>
2022-04-21 09:59:16 +0200

    Update Exer-001.py

--- CHPT-01-Intro/Exer-001.py
+++ CHPT-01-Intro/Exer-001.py
@@ -1,4 +1,6 @@
 """
+Exercise 1: Mailing Address
+
 Create a program that displays your name and complete mailing address. The address
 should be printed in the format that is normally used in the area where you live. Your
 program does not need to read any input from the user.
